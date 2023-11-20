b6be96883d4361023f3712494f54d52e85ec4a41
Qiwei Shen <106407853+willshen99@users.noreply.github.com>
2023-11-20 11:31:03 -0500

    make load_csv_to_pg into click app

--- load_csv_to_pg/load_csv_to_pg.py
+++ load_csv_to_pg/load_csv_to_pg.py
@@ -1,27 +1,31 @@
 import csv
 import psycopg2
 import os
-
-csv_file = 'load_csv_to_pg/test/input.csv'
-schema_name = 'jhuacg'
-table_name = os.path.basename(csv_file).split('.')[0]
+import click
 
 # Function to read the CSV file and analyze the header
-def analyze_csv_header(csv_file):
+def _analyze_csv_header(csv_file):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
         header = next(reader)
     return header
 
 # Function to generate DDL based on CSV header
-def generate_ddl(table_name, csv_file):
-    header = analyze_csv_header(csv_file)
+def _generate_ddl(table_name, header):
     ddl_columns = ', '.join([col + ' VARCHAR(255)' for col in header])
     ddl = f'CREATE TABLE {table_name} ({ddl_columns});'
     return ddl
 
+@click.command()
+@click.option('--schema_name', required=True, help='Schema of destination table')
+@click.option('--table_name', required=False, default='', help='Schema of destination table')
+@click.argument('csv_file', type=click.Path(exists=True))
+def generate_ddl(schema_name, table_name, csv_file):
+    if not table_name:
+        table_name = os.path.basename(csv_file).split('.')[0]
+    header = _analyze_csv_header(csv_file)
+    ddl = _generate_ddl(f'{schema_name}.{table_name}', header)
+    click.echo(ddl)
 
-# with psycopg2.connect(**db_params) as conn:
-#     cursor = conn.cursor()
-#     cursor.execute(generate_ddl(f'{schema_name}.{table_name}', header))
-#     conn.commit()
+if __name__ == '__main__':
+    generate_ddl()
